9b7cf4864a24a920c6ed33eeeedd896414c6bac7
Gonzaloned <gonzalodlv@gmail.com>
2023-09-03 21:52:30 -0300

    diag

---
+++ Ticketera/src/test.py
@@ -0,0 +1,6 @@
+import os
+
+url = "https://localhost/vendor/mike42/escpos-php/example/interface/windows-usb.php"  # Reemplaza con la URL de la página web que deseas abrir
+
+# Abre la página web en el navegador predeterminado sin mostrarlo
+os.startfile(url)
