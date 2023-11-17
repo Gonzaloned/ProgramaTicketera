ca09cb6d473365bdee160d4e881bfc50bb27a2a5
Gonzaloned <gonzalodlv@gmail.com>
2023-11-17 08:38:49 -0300

    making the database corrections to dual

--- Display/src/PantallaV9.py
+++ Display/src/PantallaV9.py
@@ -103,47 +103,34 @@ class Pantalla(object):
             self.boxAnimation(self.list_box[0])
 
     def checkAdvice(self):
-        advice_query_old= \
-        '''BEGIN TRANSACTION;
-
-        DECLARE @advice_num INT;
-        
-        -- Obtener el proximo turno disponible
-        SELECT TOP (1) @advice_num=num FROM advice ORDER BY num        
-        
-        -- Eliminar aviso
-        DELETE  FROM advice WHERE num=@advice_num
-
-        SELECT @advice_num
-
-        COMMIT TRANSACTION;
-        '''
-
         advice_query= \
-        '''
-        BEGIN TRANSACTION;
-
+        '''DECLARE @proximo_tipo INT;
+        DECLARE @numero INT;
         DECLARE @advice_num INT;
-        DECLARE @selected_counter INT;
 
-        -- Obtener el próximo aviso solicitado del contador no nulo
-        SELECT TOP (1) 
-            @advice_num = num,
-            @selected_counter = CASE
-                                    WHEN contador_tipo_CT IS NOT NULL THEN contador_tipo_CT
-                                    WHEN contador_tipo_ST IS NOT NULL THEN contador_tipo_ST
-                                END
-        FROM advice
-        ORDER BY num;
+        -- ESTA QUERY DEVUELVE EL PROXIMO NUMERO y EL TIPO
+        SELECT TOP (1) @proximo_tipo=t.tipo, @numero=t.num
+        FROM turnos_actual t INNER JOIN advice a ON t.num=a.num
+        WHERE (status=2) ORDER BY t.num;
+
+
+        -- ESTA DEVUELVE EL CONTADOR ESPECIFICO DEL TIPO Y LO GUARDA EN @advice_num
+
+        IF @proximo_tipo=1
+            SELECT TOP (1) @advice_num=contador_tipo_CT 
+            FROM turnos_actual 
+            WHERE (status=2) 
+            ORDER BY num;
+        ELSE
+            SELECT TOP (1) @advice_num=contador_tipo_ST 
+            FROM turnos_actual 
+            WHERE (status=2) 
+            ORDER BY num;    
 
         -- Eliminar aviso
-        DELETE FROM advice WHERE num = @advice_num;
+        DELETE  FROM advice WHERE num=@numero;
 
-        -- Seleccionar el contador no nulo
-        SELECT @selected_counter AS selected_counter;
-
-        COMMIT TRANSACTION;'''
-
+        SELECT @advice_num;'''
         #If query executes ok
         if (self.db_handler.queryExecution(advice_query)):  
 
